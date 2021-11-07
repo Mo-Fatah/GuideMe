2884c166c2d6ea6c60266fa53eb9c2398e11afba
mohamed <mohamedabdelfatah2027@gmail.com>
2021-11-07 17:21:53 +0200

    proper error message for unknown routes

--- app.js
+++ app.js
@@ -29,11 +29,11 @@ app.use('/api/food', RestaurantRouter);
 app.use('/api/user', userRouter);
 app.use('/api/login', loginRouter);
 app.use('/api/review', reviewRouter);
+app.get(['/login', '/food/*', '/new-restaurant', '/signup', '/profile', '/search'], (request, response) => {
+  response.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 app.get('/health', (request, response) => {
   response.send('ok');
-});
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
--- client/src/services/food.js
+++ client/src/services/food.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseUrl = '/api/food';
+const baseUrl = 'http://localhost:3003/api/food';
 
 export const getAllRests = async (params) => {
   let result;
